fadc4d7f045fba9dff08fa31dd39194ea5e495e5
andre <andreycherniak2004@gmail.com>
2024-10-24 23:09:30 +0200

    Додано в'ю для оновлення та видалення задач

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import TaskListView, TaskDetailView, TaskCreateView
+from .views import TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name='task_list'),  # Перегляд списку задач
-    path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),  # Перегляд детальної інформації
-    path('task/create/', TaskCreateView.as_view(), name='task_create'),  # Створення задачі
+    path('', TaskListView.as_view(), name='task_list'),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    path('task/create/', TaskCreateView.as_view(), name='task_create'),
+    path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),  # Оновлення задачі
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),  # Видалення задачі
 ]
+
--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,12 @@ from django.views import View
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Task
 from .forms import TaskForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
+from .models import Task
+from django.views.generic import DeleteView
+
 
 
 class TaskListView(ListView):
@@ -24,4 +30,24 @@ class TaskCreateView(CreateView):
     success_url = reverse_lazy('task_list')
 
     def form_valid(self, form):
+        form.instance.owner = self.request.user
         return super().form_valid(form)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    template_name = 'tasks/task_form.html'  # Використовуємо той самий шаблон, що й для створення задачі
+    fields = ['title', 'description', 'status', 'priority', 'deadline', 'assigned_to']
+    success_url = reverse_lazy('task_list')  # Повертаємось до списку задач після успішного оновлення
+
+    def get_queryset(self):
+        return Task.objects.filter(assigned_to=self.request.user)  # Обмежуємо доступ до задач, призначених користувачу
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('task_list')
+
+    def get_queryset(self):
+        return Task.objects.filter(assigned_to=self.request.user)
