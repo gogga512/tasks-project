7ae3b73468f23a721021389d35b1e30dba701854
andre <nenkogeorgij512@gmail.com>
2024-10-24 23:43:44 +0200

    Стилізація форм за допомогою Bootstrap

--- tasks/forms.py
+++ tasks/forms.py
@@ -7,5 +7,11 @@ class TaskForm(forms.ModelForm):
         fields = ['title', 'description', 'status', 'priority', 'deadline', 'assigned_to']
 
 class TaskFilterForm(forms.Form):
-    status = forms.ChoiceField(choices=Task.STATUS_CHOICES, required=False)
-    priority = forms.ChoiceField(choices=Task.PRIORITY_CHOICES, required=False)
+    status = forms.ChoiceField(
+        choices=Task.STATUS_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    priority = forms.ChoiceField(
+        choices=Task.PRIORITY_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
